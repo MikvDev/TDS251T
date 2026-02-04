ee2e29855ba5a772c11de28fb448f4a1de7600f3
Miguel Vargas Ferreira <miguelferreira8967@gmail.com>
2026-02-04 17:06:41 -0300

    Update imageStorage.java

--- Java/DAO e MVC/Lojinha_games/src/main/java/br/com/lojinha/util/imageStorage.java
+++ Java/DAO e MVC/Lojinha_games/src/main/java/br/com/lojinha/util/imageStorage.java
@@ -3,11 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package br.com.lojinha.util;
+import java.io.File;
+// importações para manipular arquivos de forma moderna
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+// importações para trabalho com data e horario
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-/**
+        
+ /**
  *
  * @author MIGUELVARGASFERREIRA
  */
 public class imageStorage {
+    // Define o nome da pasta onde as imagens serão salvas
+    private static final String PASTA_IMAGENS = "imagens";
+    // Método esátio para salvar image,
+    // Recebe um arquivo e retorna o caminho salvo
+    public static void main(String[] args) {
+        
+    }
     
 }
